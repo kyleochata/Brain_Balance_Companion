a981cde16ac527b715f96dccf01bbc4cf6f7e7ae
kyleochata <kyleochata@gmail.com>
2023-09-06 12:46:55 -0700

    resolved conflicts

--- assets/JS/importedData.js
+++ assets/JS/importedData.js
@@ -70,10 +70,6 @@ function getResponse(object) {
         }
         masteractivityList.push(cardInfo);
     }
-<<<<<<< HEAD
-    return masteractivityList;
-=======
->>>>>>> 2c10cccffa0f1f73af28eee30146f53b53c538ce
 }
 
 
@@ -120,79 +116,7 @@ var getNameDateStart = (nameObject) => {
                 nameCheck.startDate = value;
             }
         }
-<<<<<<< HEAD
-        namesList.push(nameCheck);
-    }
-
-    return namesList;
-}
-
-//modal JS to pop
-document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close a modal
-    function openModal($el) {
-        $el.classList.add('is-active');
-    }
-
-    function closeModal($el) {
-        $el.classList.remove('is-active');
-    }
-
-    function closeAllModals() {
-        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-            closeModal($modal);
-        });
-    }
-
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-        const modal = $trigger.dataset.target;
-        const $target = document.getElementById(modal);
-
-        $trigger.addEventListener('click', () => {
-            openModal($target);
-        });
-    });
-
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-        const $target = $close.closest('.modal');
-
-        $close.addEventListener('click', () => {
-            closeModal($target);
-        });
-    });
-
-    // Add a keyboard event to close all modals
-    document.addEventListener('keydown', (event) => {
-        if (event.code === 'Escape') {
-            closeAllModals();
-        }
-    });
-});
-
-
-//When modal button gets clicked: need to add the JS from BUlma to get modal to pop up. fxn then will take all the username data from sheets and render them in as options for the select (dropdown menu) element in modal that will pop up.
-var renderUserNameOptions = (response) => {
-
-    let listArr = getNameDateStart(response);
-    let  namesOnly= [];
-    for (let i = 0; i < listArr.length; i++) {
-        namesOnly.push(listArr[i].userName);
-    };
-    let selectNameEl = document.querySelector('#usernameSelect');
-    for (let i = 0; i < namesOnly.length; i++) {
-        let optionEl = document.createElement('option');
-        optionEl.setAttribute('value', namesOnly[i]);
-        optionEl.textContent = namesOnly[i];
-        selectNameEl.appendChild(optionEl);
-    }
-}
-//add event listener for the modal button to run the render name options for username select dropdown menu
-const usernameBtn = document.querySelector("#username");
-=======
     namesList.push(nameCheck);
     }
 }
 
->>>>>>> 2c10cccffa0f1f73af28eee30146f53b53c538ce
