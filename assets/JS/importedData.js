68d492424939f3a5e2f831156bc00c8bb4139a86
kyleochata <kyleochata@gmail.com>
2023-09-06 11:53:32 -0700

    modal html and JS

--- assets/JS/importedData.js
+++ assets/JS/importedData.js
@@ -30,62 +30,50 @@ function getResponse(object) {
         }
         for (let j = 0; j < master[0].values.length; j++) {
             let value = master[i].values[j].formattedValue
-            if (value == null)
-            { 
+            if (value == null) {
                 continue;
             }
-            if (i == 0)
-            {
-                if (value.toLowerCase().trim() == 'activity')
-                {
+            if (i == 0) {
+                if (value.toLowerCase().trim() == 'activity') {
                     activityCol = j;
                 }
-                if (value.toLowerCase().trim() == 'weeks')
-                {
+                if (value.toLowerCase().trim() == 'weeks') {
                     weekCol = j;
                 }
-                if (value.toLowerCase().trim() == 'assignment')
-                {
+                if (value.toLowerCase().trim() == 'assignment') {
                     assignmentCol = j;
                 }
-                if (value.toLowerCase().trim() == 'quantity')
-                {
+                if (value.toLowerCase().trim() == 'quantity') {
                     quantityCol = j;
                 }
-                if (value.toLowerCase().trim() == 'coach notes:')
-                {
+                if (value.toLowerCase().trim() == 'coach notes:') {
                     notesCol = j;
                 }
                 continue;
             }
-            if (j == activityCol)
-            {
+            if (j == activityCol) {
                 cardInfo.activity = value;
             }
-            if (j == weekCol)
-            {
+            if (j == weekCol) {
                 cardInfo.weeks = value;
             }
-            if (j == assignmentCol)
-            {
+            if (j == assignmentCol) {
                 cardInfo.assignment = value;
             }
-            if (j == quantityCol)
-            {
+            if (j == quantityCol) {
                 cardInfo.quantity = value;
             }
-            if (j == notesCol)
-            {
+            if (j == notesCol) {
                 cardInfo.notes = value;
             }
-            
+
         }
         masteractivityList.push(cardInfo);
         // console.log(cardInfo);
         // console.log('\n');
 
     }
-return masteractivityList;
+    return masteractivityList;
 }
 
 
@@ -93,7 +81,7 @@ return masteractivityList;
 var nameUrl = "https://sheets.googleapis.com/v4/spreadsheets/1XYp11OWdX5LIycJ07gNq6d6m13AF3ZNIhsW9xq1PKN8/?key=AIzaSyC8CJzSaxpcbUmHFLGfUkcSqTBhckWhpp0&includeGridData=true"
 axios.get(nameUrl)
     .then(response => {
-        getNameDateStart(response);
+        renderUserNameOptions(response);
     })
     .catch(error => {
         console.log(error);
@@ -110,52 +98,94 @@ var getNameDateStart = (nameObject) => {
             userName: '',
             startDate: '',
         }
-    
+
         for (let j = 0; j < nameMaster[0].values.length; j++) {
             let value = nameMaster[i].values[j].formattedValue;
             if (value == null) {
                 continue
             }
             if (i == 0) {
-                if (value.toLowerCase().trim() == 'names')
-                {
+                if (value.toLowerCase().trim() == 'names') {
                     namesCol = j;
                 }
-                if (value.toLowerCase().trim() == 'start')
-                {
+                if (value.toLowerCase().trim() == 'start') {
                     startCol = j;
                 }
                 continue;
             }
-            if (j == namesCol)
-            {
+            if (j == namesCol) {
                 nameCheck.userName = value;
             }
-            if (j == startCol)
-            {
+            if (j == startCol) {
                 nameCheck.startDate = value;
             }
+        }
+        namesList.push(nameCheck);
     }
-    namesList.push(nameCheck);
+
+    return namesList;
 }
-return namesList;
-}
+
+//modal JS to pop
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+            closeAllModals();
+        }
+    });
+});
+
 
 //When modal button gets clicked: need to add the JS from BUlma to get modal to pop up. fxn then will take all the username data from sheets and render them in as options for the select (dropdown menu) element in modal that will pop up.
-var renderUserNameOptions = () => {
+var renderUserNameOptions = (response) => {
 
-    let namesOnly = [];
-
-    for (let i=0; i < nameList.length; i++) {
-        namesOnly.push(namesList[i].userName);
+    let listArr = getNameDateStart(response);
+    let  namesOnly= [];
+    for (let i = 0; i < listArr.length; i++) {
+        namesOnly.push(listArr[i].userName);
     };
-    let selectNameEl = document.querySelector('select');
-    for (let i=0; i < namesOnly.length; i++) {
+    let selectNameEl = document.querySelector('#usernameSelect');
+    for (let i = 0; i < namesOnly.length; i++) {
         let optionEl = document.createElement('option');
-        optionEl.setAttribute('value', namesOnly[i].userName);
-        optionEl.textContent = namesOnly[i].userName;
+        optionEl.setAttribute('value', namesOnly[i]);
+        optionEl.textContent = namesOnly[i];
         selectNameEl.appendChild(optionEl);
     }
 }
-var subBtn = document.querySelector('#submit');
 //add event listener for the modal button to run the render name options for username select dropdown menu
+const usernameBtn = document.querySelector("#username");
